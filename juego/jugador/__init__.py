7bc90400538a293c424255cd132072f134f67ae4
Joaquin Blanco <blancojoaquin77@gmail.com>
2025-04-10 13:25:54 -0300

    refactor: se movio la asignación aleatorio de roles al jugador

--- juego/jugador/__init__.py
+++ juego/jugador/__init__.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from discord import User, Member
-from juego.rol import Rol
-from utils.asignar_rol import asignar_rol_aleatorio
+from juego.rol import Rol, Ciudadano, Doctor, Detective, Mafioso
+
+# from utils.asignar_rol import asignar_rol_aleatorio
+from random import Random
+from time import time
 
 
 @dataclass
@@ -9,7 +12,7 @@ class Jugador:
     rol = Rol()
 
     def seleccionar_rol(self, cantidad_de_mafiosos: int):
-        self.rol = asignar_rol_aleatorio(cantidad_de_mafiosos)
+        self.rol = self.asignar_rol_aleatorio(cantidad_de_mafiosos)
 
     def es_un_mafioso(self):
         return self.rol.es_un_mafioso()
@@ -19,6 +22,29 @@ class Jugador:
 
     async def votar_por_una_victima(self):
         return self
+
+    def asignar_rol_aleatorio(self, cantidad_de_mafiosos: int) -> Rol:
+        dados = Random()
+        dados.seed(time())
+        probabilidad_de_que_sea_doctor = (2 * cantidad_de_mafiosos) * 5  # 0, 10, 20
+        probabilidad_de_que_sea_detective = (2 * cantidad_de_mafiosos) * 5  # 0, 10, 20
+        probabilidad_de_que_sea_ciudadano = (2 * cantidad_de_mafiosos) * 10  # 0, 20, 40
+        dados = dados.randint(0, 100)
+        if dados in range(0, probabilidad_de_que_sea_ciudadano):
+            return Ciudadano()
+        if dados in range(
+            probabilidad_de_que_sea_ciudadano,
+            probabilidad_de_que_sea_ciudadano + probabilidad_de_que_sea_doctor,
+        ):
+            return Doctor()
+        if dados in range(
+            probabilidad_de_que_sea_ciudadano + probabilidad_de_que_sea_doctor,
+            probabilidad_de_que_sea_ciudadano
+            + probabilidad_de_que_sea_doctor
+            + probabilidad_de_que_sea_detective,
+        ):
+            return Detective()
+        return Mafioso()
 
 
 @dataclass
